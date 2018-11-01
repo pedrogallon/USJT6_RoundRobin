2ac2dfdbf4de377e0830985fec0b4b8401b4b2e2
jhonasbalsas <jhoonasw@hotmail.com.br>
2018-10-31 23:38:28 -0200

    Criação do bagulho caralho

--- RoundRobin/src/Controller/Main.java
+++ RoundRobin/src/Controller/Main.java
@@ -3,6 +3,7 @@ package Controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JOptionPane;
@@ -11,41 +12,48 @@ import Model.Processo;
 
 public class Main {
 	static List<Processo> fila = new ArrayList<Processo>();
-
+	static int momentoExecucao = 0;
 	public static void main(String[] args) {
 
-		int quantum = Integer.parseInt(JOptionPane.showInputDialog("Quantum: "));
+		int quantum = 0;//Integer.parseInt(JOptionPane.showInputDialog("Quantum: "));
 		// int qtd = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de
 		// Processos: "));
 		boolean continuar = true;
 		int memoria = 100;
-		while (continuar) {
-			if (1 == JOptionPane.showConfirmDialog(null, "Adicionar processo?")) {
-				continuar = false;
-			} else {
-				Processo p = new Processo(Integer.parseInt(JOptionPane.showInputDialog("Momento de Chegada: ")),
-						Integer.parseInt(JOptionPane.showInputDialog("Duração: ")));
-				memoria -= p.getDuracao();
-				if (memoria < 0) {
-					JOptionPane.showMessageDialog(null, "Espaço na memória insuficiente!");
-					continuar = false;
-				} else {
-					fila.add(p);
-				}
-			}
-		}
-
-		sort();
+//		while (continuar) {
+//			if (1 == JOptionPane.showConfirmDialog(null, "Adicionar processo?")) {
+//				continuar = false;
+//			} else {
+//				Processo p = new Processo(Integer.parseInt(JOptionPane.showInputDialog("Momento de Chegada: ")),
+//						Integer.parseInt(JOptionPane.showInputDialog("Duração: ")));
+//				memoria -= p.getDuracao();
+//				if (memoria < 0) {
+//					JOptionPane.showMessageDialog(null, "Espaço na memória insuficiente!");
+//					continuar = false;
+//				} else {
+//					fila.add(p);
+//				}
+//			}
+//		}
+		fila.add(new Processo(0, 7, "A"));
+		fila.add(new Processo(7, 2, "B"));
+		fila.add(new Processo(5, 5, "C"));
+		fila.add(new Processo(4, 3, "D"));
+		fila.add(new Processo(2, 9, "E"));
+		quantum = 6;
 		
+		sort();	
 		System.out.println(fila.toString());
-		
-		Processo p = pop();
-
-		
-
+		System.out.println("Quantum: " + quantum);
+	
+		while(!fila.isEmpty()) {
+			Processo processo = getProcesso();
+			System.out.println("Momento de chegada do processo: " + momentoExecucao);
+			momentoExecucao += processar(processo, quantum);
+		}		
 	}
 
-	private static void sort() {
+	private static void sort (){
 		Collections.sort(fila, new Comparator<Processo>() {
 			@Override
 			public int compare(Processo p1, Processo p2) {
@@ -54,11 +62,28 @@ public class Main {
 		});
 	}
 
-	private static Processo pop() {
+	private static Processo getProcesso() {
 		Processo p = fila.get(0);
 		fila.remove(0);
-		System.out.println(fila.get(0).toString());
 		return p;
 	}
+	
 
+	public static int processar(Processo processo, int quantum) {
+		if(processo.getChegada() > momentoExecucao)
+			return 1;
+		
+		System.out.println("Executando o processo: " + processo.toString());
+		if(processo.getDuracao() > quantum) {
+			processo.setDuracao(processo.getDuracao() - quantum);
+			fila.add(processo);
+			return quantum;
+		}
+		int aux = processo.getDuracao();
+		processo.setDuracao(0);
+		if(processo.getDuracao() < quantum ) 
+			return aux;
+		
+		return quantum;
+	}
 }
--- RoundRobin/src/Model/Processo.java
+++ RoundRobin/src/Model/Processo.java
@@ -1,11 +1,26 @@
 package Model;
 
 public class Processo {
-
-	int chegada;
-	int duracao;
-
 	
+	private int chegada;
+	private int duracao;
+	private String nome;
+
+	public Processo(int chegada, int duracao, String nome) {
+		this.chegada = chegada;
+		this.duracao = duracao;
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public int getChegada() {
 		return chegada;
 	}
@@ -19,16 +34,8 @@ public class Processo {
 		this.duracao = duracao;
 	}
 	
-	public Processo(int chegada, int duracao) {
-		super();
-		this.chegada = chegada;
-		this.duracao = duracao;
-	}
 	@Override
 	public String toString() {
-		return "Processo [chegada=" + chegada + ", duracao=" + duracao + "]";
+		return "Processo [chegada=" + chegada + ", duracao=" + duracao + nome + "]";
 	}
-	
-	
-	
 }
