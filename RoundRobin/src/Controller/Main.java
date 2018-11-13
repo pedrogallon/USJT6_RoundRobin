f5c3485f6d39661f89acadf7382197012be12c3e
pedro <pedro@DESKTOP-MTFG0NU>
2018-11-13 00:18:17 -0200

    testis

--- RoundRobin/src/Controller/Main.java
+++ RoundRobin/src/Controller/Main.java
@@ -12,14 +12,16 @@ import Model.Processo;
 
 public class Main {
 	static List<Processo> fila = new ArrayList<Processo>();
+	static List<Processo> filaDisco = new ArrayList<Processo>();
 	static int momentoExecucao = 0;
 	static int memoriaAtual = 0;
+	static int memoria = 100;
 
 	public static void main(String[] args) {
 		int quantum;
-		//int quantum = Integer.parseInt(JOptionPane.showInputDialog("Quantum: "));
+		// int quantum = Integer.parseInt(JOptionPane.showInputDialog("Quantum: "));
 		boolean continuar = true;
-		int memoria = 100;
+
 		// While para inserção dos Processos
 //		while (continuar) {
 //			if (1 == JOptionPane.showConfirmDialog(null, "Adicionar processo?")) {
@@ -36,7 +38,7 @@ public class Main {
 //				}
 //			}
 //		}
-		
+
 		// -- Variaveis de Teste --
 		fila.add(new Processo(0, 13, "A"));
 		fila.add(new Processo(2, 12, "B"));
@@ -45,25 +47,28 @@ public class Main {
 		fila.add(new Processo(17, 15, "E"));
 		quantum = 6;
 		sort();
-		
+
 		System.out.println("Quantum: " + quantum);
 		System.out.println("------------\n");
-		
+
 		// Processa enquanto existir processos na fila
 		while (!fila.isEmpty()) {
+
+			placeholderVerificarSeInsere(pegarProximosProcessos());
 			memoriaAtual = getMemoria();
+			
 			Processo processo = getProcesso();
 
 			momentoExecucao += processar(processo, quantum);
 		}
-		System.out.println("Momento: "+momentoExecucao+"\nExecução terminada.");
+		System.out.println("Momento: " + momentoExecucao + "\nExecução terminada.");
 	}
 
 	private static int getMemoria() {
 		int m = 0;
 		for (Processo p : fila) {
 			if (p.getChegada() <= momentoExecucao) {
-				m +=p.getDuracao();
+				m += p.getDuracao();
 			}
 		}
 		return m;
@@ -78,13 +83,14 @@ public class Main {
 			}
 		});
 	}
+
 	// Pega o primeiro processo da fila
 	private static Processo getProcesso() {
 		Processo p = fila.get(0);
 		fila.remove(0);
 		return p;
 	}
-	
+
 	// Realiza ações de acordo com a realação entre Quantum e Duração do processo
 	// Retorna um int que simboliza a quantidade de tempo de processamento
 	// (0, duracao do processo ou duracao do Quantum)
@@ -96,20 +102,39 @@ public class Main {
 			return 0;
 		}
 		System.out.print("Momento: " + momentoExecucao);
-		System.out.println("	| Memória: "+memoriaAtual);
+		System.out.println("	| Memória: " + memoriaAtual);
 		System.out.println("Executando " + processo.toString());
-		
+
 		// Caso Duração seja maior que o Quantum
 		if (processo.getDuracao() > quantum) {
 			processo.setDuracao(processo.getDuracao() - quantum);
 			fila.add(processo);
-			System.out.println("Tempo de processamento: "+quantum + "\n	--v--\n");
+			System.out.println("Tempo de processamento: " + quantum + "\n	--v--\n");
 			return quantum;
 		}
 		// Caso Duração seja menor ou igual ao Quantum
 		int aux = processo.getDuracao();
 		processo.setDuracao(0);
-		System.out.println("Tempo de processamento: "+aux + "\n	--v--\n");
+		System.out.println("Tempo de processamento: " + aux + "\n	--v--\n");
 		return aux;
+	}
+
+	public static void placeholderVerificarSeInsere(Processo[] processos) {
+		for (Processo p : processos)
+			if (p.getDuracao() + getMemoria() <= memoria) {
+				fila.add(p);
+				filaDisco.remove(0);
+			}
+	}
+
+	public static Processo[] pegarProximosProcessos() {
+		// n'ao tirar da fila ainda
+		Processo[] processos = new Processo[10];
+		for (int i = 0; i < filaDisco.size(); i++) {
+			if (filaDisco.get(i).getChegada() <= momentoExecucao) {
+				processos[i] = filaDisco.get(i);
+			}
+		}
+		return processos;
 	}
 }
